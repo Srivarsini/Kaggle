2c308ef2f03aedf20b8cb4277a8276329509b999
Akshaya Mani <akshaya@Akshayas-MacBook-Pro.local>
2017-05-27 00:49:15 -0400

    Matrix

--- Matrix.py
+++ Matrix.py
@@ -1,6 +1,9 @@
 import csv
 from scipy.cluster.hierarchy import cophenet
 from scipy.spatial.distance import pdist
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+import numpy as np
 
 with open('../mergedData.csv', 'rb') as f:
     reader = csv.reader(f)
