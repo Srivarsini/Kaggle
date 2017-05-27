39cdaf4e6fda04c6ee20f921d2d043d549ccc093
Akshaya Mani <akshaya@Akshayas-MacBook-Pro.local>
2017-05-27 00:38:01 -0400

    Matrix

--- Matrix.py
+++ Matrix.py
@@ -1,7 +1,15 @@
 import csv
+from scipy.cluster.hierarchy import cophenet
+from scipy.spatial.distance import pdist
 
 with open('../mergedData.csv', 'rb') as f:
     reader = csv.reader(f)
     data_as_list = list(reader)
 
-print data_as_list
+Z = linkage(data_as_list, 'ward')
+
+c, coph_dists = cophenet(Z, pdist(data_as_list))
+
+c
+
+
